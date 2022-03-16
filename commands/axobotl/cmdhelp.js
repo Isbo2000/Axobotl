795d1a5bc50fbbbd1416d5bd53d22176c48cc9e0
shenafield <99119344+shenafield@users.noreply.github.com>
2022-03-16 12:10:16 +0200

    Made all of the help into an asset

--- commands/axobotl/cmdhelp.js
+++ commands/axobotl/cmdhelp.js
@@ -1,4 +1,5 @@
 const config = require("../../assets/data/config.json");
+const commands = require("../../assets/help/commands.json");
 const Discord = require("discord.js");
 
 module.exports = {
@@ -6,47 +7,23 @@ module.exports = {
   callback: (message, arguments, text) => {
     //run command
 
+    const format = command => `\`${command.name}\` | ${command.description}\n`;
+
     prefix = "\u200b";
     prefix = config.prefix;
-    gencmd = [
-      "`" + prefix + "frawg` | sends a random frog image\n",
-      "`" + prefix + "axolotl` | sends a random image of an axolotl\n",
-      "`" + prefix + "updown` | sends the text back, but upsidedown\n",
-      "`" + prefix + "habr` | sends 'HAHA <word> go BRRRRRRRRRRRRRRRRRR'\n",
-      "`" + prefix + "nom <user/thing>` | eats the user or thing\n",
-      "`" + prefix + "coin` | flips an axolotl coin\n",
-    ];
+    gencmd = commands.general.map(format);
     gencmd = gencmd.join(",");
     gencmd = gencmd.replace(/[,]/g, function ($1) {
       return $1 === "," ? "" : ",";
     });
 
-    imgcmd = [
-      "`" + prefix + "suntzu <text>` | makes a fake Sun Tzu quote\n",
-      "`" + prefix + "larry <text>` | sends larry [ask a question?]\n",
-      "`" + prefix + "text <text>` | sends what you say as an image\n",
-      "`" + prefix + "crttv <image/user>` | sends image on CRTTV\n",
-    ];
+    imgcmd = commands.image.map(format);;
     imgcmd = imgcmd.join(",");
     imgcmd = imgcmd.replace(/[,]/g, function ($1) {
       return $1 === "," ? "" : ",";
     });
 
-    modcmd = [
-      "`" +
-        prefix +
-        "clear` | clears the last 49 messages(yes its exactly 49)\n",
-      "`" +
-        prefix +
-        "kick <user> <reason>` | kicks the specified user from the server\n",
-      "`" +
-        prefix +
-        "ban <user> <reason>` | bans the specified user from the server\n",
-      "`" +
-        prefix +
-        "unban <userID>` | unbans the user specified from their user ID\n",
-      "\u200B\n",
-    ];
+    modcmd = commands.moderation.map(format);;
     modcmd = modcmd.join(",");
     modcmd = modcmd.replace(/[,]/g, function ($1) {
       return $1 === "," ? "" : ",";
