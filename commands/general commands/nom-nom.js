bca56699ddaa25254cf8e3c430b60becaa1c87ad
shenafield <99119344+shenafield@users.noreply.github.com>
2022-03-16 10:57:18 +0200

    Formatted the code with prettier Added a script for running the bot

--- commands/general commands/nom-nom.js
+++ commands/general commands/nom-nom.js
@@ -1,27 +1,39 @@
 module.exports = {
-    commands: ['nom', 'eat', 'nomnom'],
-    expectedArgs: ['<user/thing>'],
-    minArgs: [1],
-    maxArgs: [1],
-    callback: (message, arguments, text) => {
-        //run command
-        user = message.author.id
-        rando = Math.floor(Math.random() * 4);
+  commands: ["nom", "eat", "nomnom"],
+  expectedArgs: ["<user/thing>"],
+  minArgs: [1],
+  maxArgs: [1],
+  callback: (message, arguments, text) => {
+    //run command
+    user = message.author.id;
+    rando = Math.floor(Math.random() * 4);
 
-        if (rando == 0) {
-            message.channel.send({content: `<@${user}> nommed on ${text}`, files: ['./assets/nomnom/nom.gif']})
-        }
+    if (rando == 0) {
+      message.channel.send({
+        content: `<@${user}> nommed on ${text}`,
+        files: ["./assets/nomnom/nom.gif"],
+      });
+    }
 
-        if (rando == 1) {
-            message.channel.send({content: `<@${user}> nommed on ${text}`, files: ['./assets/nomnom/nom2.gif']})
-        }
+    if (rando == 1) {
+      message.channel.send({
+        content: `<@${user}> nommed on ${text}`,
+        files: ["./assets/nomnom/nom2.gif"],
+      });
+    }
 
-        if (rando == 2) {
-            message.channel.send({content: `<@${user}> nommed on ${text}`, files: ['./assets/nomnom/nom3.gif']})
-        }
+    if (rando == 2) {
+      message.channel.send({
+        content: `<@${user}> nommed on ${text}`,
+        files: ["./assets/nomnom/nom3.gif"],
+      });
+    }
 
-        if (rando == 3) {
-            message.channel.send({content: `<@${user}> nommed on ${text}`, files: ['./assets/nomnom/nom4.gif']})
-        }
-    },
-}
+    if (rando == 3) {
+      message.channel.send({
+        content: `<@${user}> nommed on ${text}`,
+        files: ["./assets/nomnom/nom4.gif"],
+      });
+    }
+  },
+};
