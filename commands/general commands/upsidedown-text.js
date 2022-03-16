5d8c2122be79bc7cae119c43b3c31a24f791e403
shenafield <99119344+shenafield@users.noreply.github.com>
2022-03-16 15:49:25 +0200

    Ran everything through prettifier

--- commands/general commands/upsidedown-text.js
+++ commands/general commands/upsidedown-text.js
@@ -1,7 +1,7 @@
 let mapping = require("../../assets/updown.json");
 for (i in mapping) {
-	mapping[mapping[i]] = i;
-};
+  mapping[mapping[i]] = i;
+}
 
 module.exports = {
   commands: ["updown", "upsidedown"],
@@ -13,7 +13,9 @@ module.exports = {
     text = hi;
     let Rarray = text.split("");
     let reverseArray = Rarray.reverse();
-    let result = (reverseArray.map(character => mapping[character] ? mapping[character] : character)).join("");
+    let result = reverseArray
+      .map((character) => (mapping[character] ? mapping[character] : character))
+      .join("");
     result = result.replace("@everyone", "@еveryone"); // The first "e" here is a cyryllic letter; this prevents actually mentioning anyone
     result = result.replace("@here", "@hеre"); // Same here. They look identical but actually aren't
     result = result.replace("<@", "<\u200B@"); // To prevent mentioning unmentionable roles if the bot has admin
