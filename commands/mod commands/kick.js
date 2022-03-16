2cc87af4343321835f63c0e1deb2fe0aa415e4d8
shenafield <99119344+shenafield@users.noreply.github.com>
2022-03-16 10:57:18 +0200

    Formatted the code with prettier Added a script for running the bot

--- commands/mod commands/kick.js
+++ commands/mod commands/kick.js
@@ -1,34 +1,33 @@
 module.exports = {
-    commands: ['kick'],
-    expectedArgs: ['<user> <reason>'],
-    minArgs: [1],
-    maxArgs: [2],
-    callback: (message, arguments, text) => {
-        //run command
-        if(!message.mentions.members.first()) {
-            message.reply('Please specify a user')
-            return
-        }
+  commands: ["kick"],
+  expectedArgs: ["<user> <reason>"],
+  minArgs: [1],
+  maxArgs: [2],
+  callback: (message, arguments, text) => {
+    //run command
+    if (!message.mentions.members.first()) {
+      message.reply("Please specify a user");
+      return;
+    }
 
-        const target = message.mentions.members.first()
+    const target = message.mentions.members.first();
 
-        if (!target.kickable) {
-            message.reply('You are not able to kick this user')
-            return
-        }
+    if (!target.kickable) {
+      message.reply("You are not able to kick this user");
+      return;
+    }
 
-        arguments.shift()
-        if (arguments[0]) {
-            var reason = arguments.join(' ')
-        } else {
-            var reason = "No reason specified"
-        }
-        //const reason = arguments.join(' ')
+    arguments.shift();
+    if (arguments[0]) {
+      var reason = arguments.join(" ");
+    } else {
+      var reason = "No reason specified";
+    }
+    //const reason = arguments.join(' ')
 
-        target.kick(reason)
+    target.kick(reason);
 
-        message.channel.send(`<@${target.id}> has been kicked for: ${reason}`)
-
-    },
-    permissions: ['KICK_MEMBERS'],
-}
+    message.channel.send(`<@${target.id}> has been kicked for: ${reason}`);
+  },
+  permissions: ["KICK_MEMBERS"],
+};
