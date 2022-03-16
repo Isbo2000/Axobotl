9a25a25f0fea485f36d29d303037f3b14b7170ca
shenafield <99119344+shenafield@users.noreply.github.com>
2022-03-16 15:06:26 -0400

    Added the ping exploit prevention to this one too

--- commands/general commands/haha-go-brr.js
+++ commands/general commands/haha-go-brr.js
@@ -5,6 +5,9 @@ module.exports = {
   maxArgs: [1],
   callback: (message, arguments, text) => {
     //run command
+    text = text.replace("@everyone", "@еveryone"); // The first "e" here is a cyryllic letter; this prevents actually mentioning anyone
+    text = text.replace("@here", "@hеre"); // Same here. They look identical but actually aren't
+    text = text.replace("<@", "<\u200B@"); // To prevent mentioning unmentionable roles if the bot has admin
     message.channel.send("HAHA " + text + " go BRRRRRRRRRRRRRRRRRR");
   },
 };
