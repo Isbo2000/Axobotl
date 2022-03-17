a800509bf6f23aa35f8f57155ed10ea1481e25ec
shenafield <99119344+shenafield@users.noreply.github.com>
2022-03-17 11:37:49 +0200

    Moved most of the help - notably the graphic - into an asset Also moved the invite client ID into config.json

--- commands/axobotl/help.js
+++ commands/axobotl/help.js
@@ -1,43 +1,32 @@
 const config = require("../../assets/data/config.json");
 const embed_gen = require("../../utils/embed.js");
+const asset = require("../../assets/help/help.json");
 
 module.exports = {
   commands: ["help", "help menu", "menu"],
   callback: (message, arguments, text) => {
     //run command
 
-    prefix = "\u200b";
     prefix = config.prefix;
-    help = [
-      "≽(^ ᗜ ^)≼       |------------------------------------|",
-      "     /(          )/< >|      A multi-purpose axolotl bot       |",
-      "         (         )               |                   Made by Isbo2000                    |",
-      "         (        )                |------------------------------------|",
-    ];
-    help = help.join("\n");
 
-    other = [
-      "Type this for the list of commands:",
-      "`" + prefix + "commands`\n",
-      "Type this to see the credits:",
-      "`" + prefix + "credits`",
-    ];
-    other = other.join("\n");
+    // Help commands
+    commands = asset.commands
+      .map(
+        (command) => `${command.description}:\n\`${prefix}${command.command}\``
+      )
+      .join("\n");
 
-    invite = [
-      "Link to invite this bot to your server:",
-      "**[Invite Me!](https://discord.com/api/oauth2/authorize?client_id=881548943132098591&permissions=1374393199687&scope=bot)**",
-      "\u200B\n",
-    ];
-    invite = invite.join("\n");
+    invite_link = `https://discord.com/api/oauth2/authorize?client_id=${config.client}&permissions=${config.permissions}&scope=bot`;
+    // For now the scope is just "bot" since we don't yet have slash commands implemented. yet.
+    invite = `Link to invite this bot to your server:\n**[Invite Me!](${invite_link})**`;
 
     const embed = embed_gen
       .embed()
       .setTitle("**Help Menu**")
       .addFields(
-        { name: "\u200B", value: help },
-        { name: "\u200B\nHelp:", value: other },
-        { name: "\u200B\nInvite:", value: invite }
+        { name: "\u200B", value: asset.graphic.join("\n") },
+        { name: "Help:", value: commands },
+        { name: "Invite:", value: invite }
       );
     message.channel.send({ embeds: [embed] });
   },
