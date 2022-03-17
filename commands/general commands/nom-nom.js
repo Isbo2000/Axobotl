67ae3f29ed7326a663c07986f47b3e494e3941a6
shenafield <99119344+shenafield@users.noreply.github.com>
2022-03-17 07:50:41 +0200

    Added mention protection to nomnom for all roles, everyone and here but not people If there's at least one role mention, all mentions will be blocked, but it should be okay because you can't mention more than one thing

--- commands/general commands/nom-nom.js
+++ commands/general commands/nom-nom.js
@@ -8,10 +8,20 @@ module.exports = {
   callback: (message, arguments, text) => {
     //run command
     user = message.author.id;
+    console.log(text);
     rando = Math.floor(Math.random() * assets.length);
 
+    let result = text;
+    result = result.replaceAll("@everyone", "@еveryone"); // The first "e" here is a cyryllic letter; this prevents actually mentioning anyone
+    result = result.replaceAll("@here", "@hеre"); // Same here. They look identical but actually aren't
+    if (message.mentions.roles.size) {
+      // if at least one role was mentioned, we're going to bleck everything
+      result = result.replaceAll("<@", "<\u200B@"); // To prevent mentioning unmentionable roles if the bot has admin
+    }
+    result = `<@${user}> nommed on ${result}`;
+
     message.channel.send({
-      content: `<@${user}> nommed on ${text}`,
+      content: result,
       files: [assets[rando]],
     });
   },
