495b7e6c61a7ce38e76228eb273c8f3b4ee43ce0
Isbo <94263966+Isbo2000@users.noreply.github.com>
2022-03-20 17:04:17 -0400

    Update clear.js

--- commands/mod commands/clear.js
+++ commands/mod commands/clear.js
@@ -3,7 +3,7 @@ module.exports = {
   expectedArgs: ["<number of messages>"],
   minArgs: [1],
   maxArgs: [1],
-  callback: async (message, arguments, text) => {
+  callback: (message, arguments, text) => {
     //run command
     
     num = parseInt(text)
@@ -11,13 +11,12 @@ module.exports = {
     if (!isNaN(num)) {
       Math.abs(num)
       if (num > 0 && num < 101) {
-        await message.delete()
+        message.delete()
         message.channel.bulkDelete(num).catch((error) => {
           if (error.code == !10008) {
             message.channel.send(
               "Cannot clear, as there are messages older than 2 weeks"
             );
-            return
           }
         });
         message.channel.send(num+" messages successfully deleted")
