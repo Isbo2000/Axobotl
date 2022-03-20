f074efe0b57fe61379817be5b548332cd8442c94
Isbo <94263966+Isbo2000@users.noreply.github.com>
2022-03-19 21:29:33 -0400

    fixed up the clear messages, and moved embed.js to data

---
+++ assets/data/embed.js
@@ -0,0 +1,11 @@
+// This contains the script for generating an embed and that's it
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+module.exports = {
+  embed: () =>
+    new Discord.MessageEmbed()
+      .setColor("#00a4ff")
+      .setDescription(`prefix: \`${config.prefix}\``)
+      .EmbedFooterData("Axobotl   |   Version: " + config.version),
+};
