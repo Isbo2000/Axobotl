ba0f88b5cb184d5e7be2ba90dd985587c11a6a7b
shenafield <99119344+shenafield@users.noreply.github.com>
2022-03-22 12:45:52 +0200

    Finished the initial transition of image commands to typescript

--- commands/Image commands/text.ts
+++ commands/Image commands/text.ts
@@ -1,43 +1,41 @@
-const { MessageAttachment } = require("discord.js");
-const Canvas = require("canvas");
+import { MessageAttachment } from "discord.js";
+import Canvas from "canvas";
 
-module.exports = {
-  commands: ["text"],
-  expectedArgs: ["<text>"],
-  minArgs: [1],
-  callback: async (message, arguments, text) => {
-    //run command
-    const applyText = (canvas, text) => {
-      const context = canvas.getContext("2d");
-
-      let fontSize = 100;
-
-      do {
-        context.font = `${(fontSize -= 5)}px sans-serif`;
-      } while (context.measureText(text).width > canvas.width - 50);
-
-      if (fontSize < 5) {
-        context.font = "5px sans-serif";
-      }
-
-      return context.font;
-    };
-    const canvas = Canvas.createCanvas(1000, 250);
+export const commands = ["text"];
+export const expectedArgs = ["<text>"];
+export const minArgs = [1];
+export const callback = async (message, args, text) => {
+  //run command
+  const applyText = (canvas, text) => {
     const context = canvas.getContext("2d");
-    const background = await Canvas.loadImage(
-      "./assets/imagecommands/text/text.png"
-    );
 
-    context.drawImage(background, 0, 0, canvas.width, canvas.height);
-    context.strokeStyle = "#ffffff00";
-    context.strokeRect(0, 0, canvas.width, canvas.height);
-    context.font = applyText(canvas, text);
-    context.textAlign = "left";
-    context.fillStyle = "#ffffff";
-    context.fillText(text, 50, 125);
+    let fontSize = 100;
 
-    const attachment = new MessageAttachment(canvas.toBuffer(), "Text.png");
+    do {
+      context.font = `${(fontSize -= 5)}px sans-serif`;
+    } while (context.measureText(text).width > canvas.width - 50);
 
-    message.channel.send({ files: [attachment] });
-  },
+    if (fontSize < 5) {
+      context.font = "5px sans-serif";
+    }
+
+    return context.font;
+  };
+  const canvas = Canvas.createCanvas(1000, 250);
+  const context = canvas.getContext("2d");
+  const background = await Canvas.loadImage(
+    "./assets/imagecommands/text/text.png"
+  );
+
+  context.drawImage(background, 0, 0, canvas.width, canvas.height);
+  context.strokeStyle = "#ffffff00";
+  context.strokeRect(0, 0, canvas.width, canvas.height);
+  context.font = applyText(canvas, text);
+  context.textAlign = "left";
+  context.fillStyle = "#ffffff";
+  context.fillText(text, 50, 125);
+
+  const attachment = new MessageAttachment(canvas.toBuffer(), "Text.png");
+
+  message.channel.send({ files: [attachment] });
 };
