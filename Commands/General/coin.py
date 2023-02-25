8f3c29ad5883ffc1741a1c0a7ecfa46b55629a41
Isbo2000 <94263966+Isbo2000@users.noreply.github.com>
2023-02-25 11:27:15 -0500

    added coin command

---
+++ Commands/General/coin.py
@@ -0,0 +1,75 @@
+from discord.ext import commands
+import discord
+import asyncio
+import random
+import json
+
+with open('./Assets/config.json') as cfg:
+    config = json.load(cfg)
+
+class Coin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @discord.slash_command(name="coin",description="Flips an axolotl coin")
+    async def coin(self, ctx: discord.ApplicationContext):
+
+        invite = f"https://discord.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions={config['permissions']}&scope=applications.commands%20bot"
+        server = f"https://discord.gg/{config['server']}"
+
+        async def heads(message):
+            heads = discord.Embed(
+                title="**Heads**",
+                description=f"\n[Invite Me!]({invite})   |   [Join Support Server!]({server})",
+                color=discord.Color.from_rgb(0,164,255)
+            )
+
+            heads.set_image(url="https://i.imgur.com/O7JwjS5.png")
+
+            heads.set_footer(
+                text=f"{self.bot.user.name}   |   Version: {config['version']}",
+                icon_url=self.bot.user.avatar
+            )
+
+            await message.edit_original_response(embed=heads)
+        
+        async def tails(message):
+            tails = discord.Embed(
+                title="**Tails**",
+                description=f"\n[Invite Me!]({invite})   |   [Join Support Server!]({server})",
+                color=discord.Color.from_rgb(0,164,255)
+            )
+
+            tails.set_image(url="https://i.imgur.com/KjNZAjk.png")
+
+            tails.set_footer(
+                text=f"{self.bot.user.name}   |   Version: {config['version']}",
+                icon_url=self.bot.user.avatar
+            )
+
+            await message.edit_original_response(embed=tails)
+
+        flipping = discord.Embed(
+            title="***Flipping***",
+            description=f"\n[Invite Me!]({invite})   |   [Join Support Server!]({server})",
+            color=discord.Color.from_rgb(0,164,255)
+        )
+
+        flipping.set_image(url="https://i.imgur.com/woPnNga.gif")
+
+        flipping.set_footer(
+            text=f"{self.bot.user.name}   |   Version: {config['version']}",
+            icon_url=self.bot.user.avatar
+        )
+
+        message = await ctx.respond(embed=flipping)
+
+        await asyncio.sleep(3)
+
+        flip = random.randrange(0,2)
+        if (flip == 0): await heads(message)
+        elif (flip == 1): await tails(message)
+
+
+def setup(bot):
+    bot.add_cog(Coin(bot))
