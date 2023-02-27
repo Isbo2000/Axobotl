5a11bdec2d717c5edd2727dbaf704a4243ed5983
Isbo2000 <94263966+Isbo2000@users.noreply.github.com>
2023-02-27 12:03:44 -0500

    added frog command

---
+++ Commands/Animals/frog.py
@@ -0,0 +1,41 @@
+from discord.ext import commands
+import discord
+import random
+import json
+
+with open('./Assets/config.json') as cfg:
+    config = json.load(cfg)
+
+with open('./Assets/Commands/frog.json') as img:
+    images = json.load(img)
+
+class Frog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @discord.slash_command(name="frog",description="Sends a random frog image")
+    async def frog(self, ctx: discord.ApplicationContext):
+
+        invite = f"https://discord.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions={config['permissions']}&scope=applications.commands%20bot"
+        server = f"https://discord.gg/{config['server']}"
+
+        frog = random.choice(images)
+
+        embed = discord.Embed(
+            title="**Have a random frog!**",
+            description=f"[Invite Me!]({invite})   |   [Join Server!]({server})",
+            color=discord.Color.from_rgb(0,164,255)
+        )
+        
+        embed.set_image(url=frog)
+
+        embed.set_footer(
+            text=f"{self.bot.user.name}   |   Version: {config['version']}",
+            icon_url=self.bot.user.avatar
+        )
+
+        await ctx.respond(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Frog(bot))
