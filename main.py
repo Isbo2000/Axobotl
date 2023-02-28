fbf83593f3cae413e55319d2afb00e368511264b
Isbo2000 <94263966+Isbo2000@users.noreply.github.com>
2023-02-27 20:50:07 -0500

    added cooldown message

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 print("|\nInitializing...")
 
+from discord.ext import commands
+import datetime
 import discord
 import pwinput
 import json
@@ -47,54 +49,76 @@ async def after_invoke(ctx: discord.ApplicationContext):
 
 @bot.event
 async def on_application_command_error(ctx: discord.ApplicationContext, error: discord.DiscordException):
-    print(f"\nERROR: {error}\n")
+    if isinstance(error, commands.CommandOnCooldown):
+        invite = f"https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions={config['permissions']}&scope=applications.commands%20bot"
+        server = f"https://discord.gg/{config['server']}"
 
-    dmlog = await bot.fetch_user(bot.owner_id)
-
-    if ctx.guild: place = ctx.guild.name
-    else: place = "DMs"
-
-    options = "**Selected:**"
-    for option in ctx.selected_options if ctx.selected_options else [None]:
-        options = "\n".join([
-            options,
-            f"{option['name']}: `{str(option['value'])}`" if option else "None"
-        ])
-    
-    options = "\n".join([options,"**Not selected:**\n"])
-    for option in ctx.unselected_options if ctx.unselected_options else [None]:
-        options = (
-            ", ".join([options,str(option.name)])
-        ) if option and not options.endswith("\n") else (
-            "".join([options,str(option.name)])
-        ) if option else (
-            "".join([options, "None"])
+        embed = discord.Embed(
+            title="This command is on cooldown! :(",
+            description=f"You can use {ctx.command.mention} again <t:{int(str(datetime.datetime.now().timestamp()).split('.')[0]) + int(str(ctx.command.cooldown.get_retry_after()).split('.')[0])}:R>",
+            color=discord.Color.from_rgb(255,0,0)
         )
 
-    embed = discord.Embed(
-        title="**Command Error**",
-        description=f"**Command:**\n{ctx.command.mention}\n**Place:**\n{place}\n{options}",
-        color=discord.Color.from_rgb(255,0,0)
-    )
+        embed.add_field(
+            name=f"The cooldown is `{str(ctx.command.cooldown.per).split('.')[0]} seconds`",
+            value=f"[Invite Me!]({invite})   |   [Join Server!]({server})"
+        )
 
-    embed.set_author(
-        name=f"{ctx.author}",
-        url=f"https://www.discord.com/users/{ctx.author.id}/",
-        icon_url=ctx.author.avatar
-    )
+        embed.set_footer(
+            text=f"{bot.user.name}   |   Version: {config['version']}",
+            icon_url=bot.user.avatar
+        )
 
-    embed.add_field(
-        name="Error:",
-        value=str(error).replace("Application Command raised an exception:",""),
-        inline=False
-    )
+        await ctx.respond(embed=embed,ephemeral=True)
+    else:
+        print(f"\nERROR: {error}\n")
 
-    embed.set_footer(
-        text=f"{bot.user.name}   |   Version: {config['version']}",
-        icon_url=bot.user.avatar
-    )
+        dmlog = await bot.fetch_user(bot.owner_id)
 
-    await dmlog.send(embed=embed)
+        if ctx.guild: place = ctx.guild.name
+        else: place = "DMs"
+
+        options = "**Selected:**"
+        for option in ctx.selected_options if ctx.selected_options else [None]:
+            options = "\n".join([
+                options,
+                f"{option['name']}: `{str(option['value'])}`" if option else "None"
+            ])
+        
+        options = "\n".join([options,"**Not selected:**\n"])
+        for option in ctx.unselected_options if ctx.unselected_options else [None]:
+            options = (
+                ", ".join([options,str(option.name)])
+            ) if option and not options.endswith("\n") else (
+                "".join([options,str(option.name)])
+            ) if option else (
+                "".join([options, "None"])
+            )
+
+        embed = discord.Embed(
+            title="**Command Error**",
+            description=f"**Command:**\n{ctx.command.mention}\n**Place:**\n{place}\n{options}",
+            color=discord.Color.from_rgb(255,0,0)
+        )
+
+        embed.set_author(
+            name=f"{ctx.author}",
+            url=f"https://www.discord.com/users/{ctx.author.id}/",
+            icon_url=ctx.author.avatar
+        )
+
+        embed.add_field(
+            name="Error:",
+            value=str(error).replace("Application Command raised an exception:",""),
+            inline=False
+        )
+
+        embed.set_footer(
+            text=f"{bot.user.name}   |   Version: {config['version']}",
+            icon_url=bot.user.avatar
+        )
+
+        await dmlog.send(embed=embed)
 
 try:
     print("|\nChecking for token...")
