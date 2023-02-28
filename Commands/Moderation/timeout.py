f1684902e2649f211242e654b9b9b5cc0d03c76b
Isbo2000 <94263966+Isbo2000@users.noreply.github.com>
2023-02-28 18:22:13 -0500

    added timeout command

---
+++ Commands/Moderation/timeout.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+import datetime
+import discord
+import Modules
+
+class Timeout(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @discord.command(name="timeout",description="Timeout a user (default 10 minutes)")
+    @commands.has_permissions(moderate_members=True)
+    @commands.bot_has_permissions(moderate_members=True)
+    @discord.option(name="user",description="Enter a user to timeout",required=True)
+    @discord.option(name="minutes",description="Enter the amount of minutes to timeout a given user",required=False)
+    @discord.option(name="reason",description="Enter a reason for the timeout",required=False)
+    async def timeout(self, ctx: discord.ApplicationContext, user: discord.Member, minutes: int = 10, reason: str = "No reason given"):
+        duration = datetime.timedelta(minutes=minutes)
+
+        await user.timeout_for(duration=duration,reason=reason)
+
+        title = f"Timed out {user} for {minutes} minutes"
+        description = f"**Reason:** {reason}"
+
+        await Modules.Embeds(self.bot,title=title,description=description).respond(ctx,True)
+
+def setup(bot):
+    bot.add_cog(Timeout(bot))
