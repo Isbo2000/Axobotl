5df042abaf7a4f2ddb46cdc6cb476e2b40e068a6
Isbo2000 <94263966+Isbo2000@users.noreply.github.com>
2023-03-07 17:01:50 -0500

    changed coin command to use local files

--- Commands/General/coin.py
+++ Commands/General/coin.py
@@ -8,9 +8,6 @@ import json
 with open('./Assets/config.json') as cfg:
     config = json.load(cfg)
 
-with open('./Assets/Commands/coin.json') as img:
-    images = json.load(img)
-
 class Coin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -18,15 +15,23 @@ class Coin(commands.Cog):
     @discord.slash_command(name="coin",description="Flips an axolotl coin")
     @commands.cooldown(1,config['cooldown'],commands.BucketType.member)
     async def coin(self, ctx: discord.ApplicationContext):
-        msg = await Modules.Embeds(self.bot,title="***Flipping***",image=images['flipping']).respond(ctx)
+        await ctx.defer()
+
+        coin = discord.File("./Assets/Commands/coin/coin.gif", "Coin.gif")
+        heads = discord.File("./Assets/Commands/coin/heads.png", "Heads.png")
+        tails = discord.File("./Assets/Commands/coin/tails.png", "Tails.png")
+
+        msg = await Modules.Embeds(self.bot,title="***Flipping***",file=coin,color=None).respond(ctx)
 
         await asyncio.sleep(3)
 
         flip = random.randrange(0,2)
+
         if (flip == 0):
-            await Modules.Embeds(self.bot,title="**Heads**",image=images['heads']).edit(msg)
+            await Modules.Embeds(self.bot,title="**Heads**",file=heads).edit(msg)
+
         elif (flip == 1):
-            await Modules.Embeds(self.bot,title="**Tails**",image=images['tails']).edit(msg)
+            await Modules.Embeds(self.bot,title="**Tails**",file=tails).edit(msg)
 
 
 def setup(bot):
