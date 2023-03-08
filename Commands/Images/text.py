9069efcaf15143fd8fb3b69824e9cdbbc089825d
Isbo2000 <94263966+Isbo2000@users.noreply.github.com>
2023-03-07 20:03:57 -0500

    localized? font

--- Commands/Images/text.py
+++ Commands/Images/text.py
@@ -16,17 +16,19 @@ class Text(commands.Cog):
         with Image.open("./Assets/Commands/text/text.png") as image:
             image.load()
         image.copy()
+
+        font = "./Assets/Fonts/Questrial-Regular.ttf"
         
         testim = Image.new('RGB', (image.width, image.height))
 
-        width = ImageDraw.Draw(testim).textbbox((50,50), text, font=ImageFont.truetype("arial",100))[2]
+        width = ImageDraw.Draw(testim).textbbox((50,50), text, font=ImageFont.truetype(font,100))[2]
         size = round(100 / (width / image.width) * 0.90)
         size = size if size < 250 else 250
 
-        height = image.height - ImageDraw.Draw(testim).textbbox((50,50), text, font=ImageFont.truetype("arial",size))[3]
+        height = image.height - ImageDraw.Draw(testim).textbbox((50,50), text, font=ImageFont.truetype(font,size))[3]
         height = round((height / 2) if height > 0 else height)
 
-        ImageDraw.Draw(image).text((50,height), text, font=ImageFont.truetype("arial",size))
+        ImageDraw.Draw(image).text((50,height), text, font=ImageFont.truetype(font,size))
 
         buffer = io.BytesIO()
         image.save(buffer,"PNG")
