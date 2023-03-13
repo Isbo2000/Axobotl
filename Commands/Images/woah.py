f34b490eb3f0a463df2530f0a49b97c359332264
Isbo2000 <94263966+Isbo2000@users.noreply.github.com>
2023-03-13 12:53:50 -0400

    added woah command

---
+++ Commands/Images/woah.py
@@ -0,0 +1,32 @@
+from discord.ext import commands
+from PIL import Image, ImageDraw, ImageFont
+import discord
+import Modules
+import io
+
+class Woah(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @discord.slash_command(name="woah",description="Puts a given string of text and image onto a 'woah reaction meme'")
+    @discord.option(name="text",description="Enter text to use",required=True)
+    @discord.option(name="attachment",description="Attach image to use",required=True)
+    async def woah(self, ctx: discord.ApplicationContext, text: str, attachment: discord.Attachment):
+        await ctx.defer()
+
+        bg = await Modules.Images.load("./Assets/Commands/woah/woah.png")
+
+        Modules.Images.add_textbox(bg,text,(0,0,0),(-40,2),(400,100),max_size=100,target_size=10)
+
+        image = await Modules.Images.load(attachment)
+
+        image = image.resize((225,190))
+
+        bg.paste(image,(350,375))
+
+        file = Modules.Images.save(bg,"Woah.png")
+
+        await Modules.Embeds(self.bot,file=file).respond(ctx)
+
+def setup(bot):
+    bot.add_cog(Woah(bot))
