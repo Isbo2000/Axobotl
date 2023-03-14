04be5bd0defd3b61cff35187268b3c3cfc10f39a
Isbo2000 <94263966+Isbo2000@users.noreply.github.com>
2023-03-13 21:21:13 -0400

    added crttv command

---
+++ Commands/Images/crttv.py
@@ -0,0 +1,37 @@
+from discord.ext import commands
+from PIL import Image, ImageDraw, ImageFont
+import discord
+import Modules
+import io
+
+class Crttv(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @discord.slash_command(name="crttv",description="Puts a given image onto a crttv")
+    @discord.option(name="attachment",description="Attach image to use",required=True)
+    async def crttv(self, ctx: discord.ApplicationContext, attachment: discord.Attachment):
+        await ctx.defer()
+
+        bg = await Modules.Images.load("./Assets/Commands/crttv/bg.png")
+
+        fg = await Modules.Images.load("./Assets/Commands/crttv/fg.png")
+
+        image = await Modules.Images.load(attachment)
+
+        image = image.resize((255,200))
+
+        try:
+            bg.paste(image,(85,105),image)
+        
+        except ValueError:
+            bg.paste(image,(85,105))
+        
+        bg.paste(fg,(37,39),fg)
+
+        file = Modules.Images.save(bg,"CRTTV.png")
+
+        await Modules.Embeds(self.bot,file=file).respond(ctx)
+
+def setup(bot):
+    bot.add_cog(Crttv(bot))
